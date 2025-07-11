75ed042c6129b3493695f6fd682be076723b03d7
admin <admin@datavirtuality.de>
2025-07-11 10:42:15 +0000

    Commit message

---
+++ jobs/parameters/jobEnable.sql
@@ -0,0 +1,10 @@
+
+call SYSADMIN.enableSchedulerJob(uuid => '260e8924-50c4-11ee-bb50-000000000000', enabled => true);;
+call SYSADMIN.enableSchedulerJob(uuid => '5a709f94-50c4-11ee-bb50-000000000000', enabled => true);;
+call SYSADMIN.enableSchedulerJob(uuid => 'c2f355aa-50c4-11ee-bb50-000000000000', enabled => true);;
+call SYSADMIN.enableSchedulerJob(uuid => 'c7b066c8-50c4-11ee-bb50-000000000000', enabled => true);;
+call SYSADMIN.enableSchedulerJob(uuid => '7919ee7e-50c4-11ee-bb50-000000000000', enabled => true);;
+call SYSADMIN.enableSchedulerJob(uuid => '4e80f17e-50c4-11ee-bb50-000000000000', enabled => true);;
+call SYSADMIN.enableSchedulerJob(uuid => '6fb56dfd-50c4-11ee-bb50-000000000000', enabled => true);;
+call SYSADMIN.enableSchedulerJob(uuid => 'dd3dfd2c-50c4-11ee-bb50-000000000000', enabled => true);;
+call SYSADMIN.enableSchedulerJob(uuid => '67ed32e1-50c4-11ee-bb50-000000000000', enabled => true);;
---
+++ jobs/parameters/jobUsers.sql
@@ -0,0 +1,10 @@
+
+call SYSADMIN.changeJobParameters(jobUuid => '260e8924-50c4-11ee-bb50-000000000000', "owner" => 'system', "runAs" => 'CALLER');;
+call SYSADMIN.changeJobParameters(jobUuid => '5a709f94-50c4-11ee-bb50-000000000000', "owner" => 'system', "runAs" => 'CALLER');;
+call SYSADMIN.changeJobParameters(jobUuid => 'c2f355aa-50c4-11ee-bb50-000000000000', "owner" => 'system', "runAs" => 'CALLER');;
+call SYSADMIN.changeJobParameters(jobUuid => 'c7b066c8-50c4-11ee-bb50-000000000000', "owner" => 'system', "runAs" => 'CALLER');;
+call SYSADMIN.changeJobParameters(jobUuid => '7919ee7e-50c4-11ee-bb50-000000000000', "owner" => 'system', "runAs" => 'CALLER');;
+call SYSADMIN.changeJobParameters(jobUuid => '4e80f17e-50c4-11ee-bb50-000000000000', "owner" => 'system', "runAs" => 'CALLER');;
+call SYSADMIN.changeJobParameters(jobUuid => '6fb56dfd-50c4-11ee-bb50-000000000000', "owner" => 'system', "runAs" => 'CALLER');;
+call SYSADMIN.changeJobParameters(jobUuid => 'dd3dfd2c-50c4-11ee-bb50-000000000000', "owner" => 'system', "runAs" => 'CALLER');;
+call SYSADMIN.changeJobParameters(jobUuid => '67ed32e1-50c4-11ee-bb50-000000000000', "owner" => 'system', "runAs" => 'CALLER');;
